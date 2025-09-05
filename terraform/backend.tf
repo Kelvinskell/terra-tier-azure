26a665c52075f62d5b2fff28297ffc7ecbff62d2
Kelvin Onuchukwu <kelvinrashid139@gmail.com>
2025-09-05 16:14:55 +0200

    update comment

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -16,7 +16,7 @@ terraform {
 
 ##########################################################################################
 #YOU MUST CHANGE THE ABOVE BACKEND TO YOUR OWN BACKEND IF YOU WANT THIS TO WORK FOR YOU.
-#YOU CAN EITHER CREATE A REMOTE BACKEND ON TERRAFORM CLOUD OR WITH AN Azure storage account.
+#YOU CAN EITHER CREATE A REMOTE BACKEND ON TERRAFORM CLOUD OR WITH AN AZURE STORAGE ACCOUNT.
 #ALTERNATIVELY, YOU CAN SIMPLY CHOOSE TO USE A LOCAL BACKEND.
 #EITHER WAY, YOU MUST CHANGE THIS BACKEND.
 ###########################################################################################
