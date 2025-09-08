b73eef1680468ea3b6f11a7d378e8675f0a03d57
Kelvin Onuchukwu <kelvinrashid139@gmail.com>
2025-09-08 18:00:48 +0200

    Create provider for random pass generation

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -5,6 +5,12 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.43.0"
     }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7.2"
+    }
+
   }
   cloud {
     organization = "Org101"
