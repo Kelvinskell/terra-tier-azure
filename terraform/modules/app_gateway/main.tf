641bfbf2b7302459a3a33a7bd947ab0ba6d23433
Kelvin Onuchukwu <kelvinskelll@gmail.com>
2025-09-09 13:23:27 +0200

    Update main.tf

---
+++ terraform/modules/app_gateway/main.tf
@@ -0,0 +1,63 @@
+# Create application gateway
+resource "azurerm_application_gateway" "network" {
+  name                = "example-appgateway"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+
+  sku {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "my-gateway-ip-configuration"
+    subnet_id = module.network.private_subnet_id
+  }
+
+  frontend_port {
+    name = local.frontend_port_name
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+
+  backend_address_pool {
+    name = local.backend_address_pool_name
+  }
+
+  backend_http_settings {
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    port                  = 5000    # traffic forwarded to backend on 5000
+    protocol              = "Http"
+    request_timeout       = 60
+  }
+
+  http_listener {
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = local.request_routing_rule_name
+    priority                   = 9
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
+  }
+}
+
+# Attach App Gw to VMSS
+resource "azurerm_application_gateway_backend_address_pool_address" "vmss" {
+  application_gateway_name     = azurerm_application_gateway.network.name
+  resource_group_name          = azurerm_resource_group.example.name
+  backend_address_pool_name    = local.backend_address_pool_name
+  virtual_machine_scale_set_id = azurerm_linux_virtual_machine_scale_set.example.id
+}
